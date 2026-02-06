2c940254b0ea0e9f9deb2ed9f7742bc00accce78
ShashwatUpadhayay <shashwatupadhyay254@gmail.com>
2026-02-06 19:07:55 +0530

    if else

---
+++ api/hello/IF-ELSE/2.go
@@ -0,0 +1,23 @@
+// Control Statements
+
+package main
+
+import "fmt"
+
+func main() {
+	// a := 500
+	// if a > 200 {
+	// 	fmt.Printf("Value of a is greater than 200\n")
+	// }
+	// fmt.Printf("Value of a is %d\n", a)
+
+	var v1 int = 50
+	v2 := 100
+
+	if v1 == 50 {
+		if v2 == 100 {
+			fmt.Printf("Value of v1 is 50 and Value of v2 is 100\n")
+		}
+
+	}
+}
